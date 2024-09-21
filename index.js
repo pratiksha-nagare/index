d88cde16fcddb166b914978a91bcaaead23cc732
pratiksha-nagare <nagarebabasaheb4@gmail.com>
2024-09-21 15:57:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,109 @@
+// function add(){
+//     var inputfield =document.querySelector(".box").value;
+//     var container = document.querySelector('.container');
+
+//      var h3Element = document.createElement('h3');
+//      h3Element .textContent=inputfield;
+
+//     container.appendChild(h3Element);
+
+// }
+
+
+
+
+
+     var Object =[{
+            task:"hello"
+        },
+        {
+            task:"world"
+        },
+        {
+            task:"dream"
+        }
+        ,{
+            task:"Guider"
+        },
+        
+        {
+            task:"pratu"
+        },
+        {
+            task:"manu"
+        },
+        {
+            task:"nik"
+        }
+        ,{
+            task:"sonu"
+        },
+
+        {
+            task:"hello"
+        },
+        {
+            task:"world"
+        },
+        {
+            task:"dream"
+        }
+        ,{
+            task:"Guider"
+        },
+
+        {
+            task:"hello"
+        },
+        {
+            task:"world"
+        },
+        {
+            task:"dream"
+        }
+        ,{
+            task:"Guider"
+        },
+
+        {
+            task:"hello"
+        },
+        {
+            task:"world"
+        },
+        {
+            task:"dream"
+        }
+        ,{
+            task:"Guider"
+        },
+    ]
+
+
+     Object.forEach(serve =>{
+    
+    var container=document.querySelector('.container');
+    var inputfield=document.querySelector('.inputfield');
+
+    
+
+    var h3Element = document.createElement("h3");
+    h3Element .textContent=serve.task;
+
+    
+    container.appendChild(h3Element);
+
+   })
+
+
+
+
+
+
+
+
+
+
+
+
+
